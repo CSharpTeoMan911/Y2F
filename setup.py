a4bd7874f60215b44f78503fedfb96f4a4c3e573
CSharpTeoMan911 <teodormihail07@gmail.com>
2023-09-01 20:12:06 +0100

    * MINOR CHANGE

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='Y2F',
-    version='1.2',
+    version='1.3',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'Y2F=Y2F_PACKAGE.main:main',
         ]
     },
+    description = 'Command line utility for downloading YouTube videos in MP3 or MP4 format.',
+    author_email = 'teodormihail07@gmail.com',
+    url = 'https://github.com/CSharpTeoMan911/Y2F', 
     install_requires=[
         'pytube'
     ]
